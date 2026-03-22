e0c10a0e66785c02fa742bb13c6379f883c23317
Zax4r <zaharbedarik@gmail.com>
2026-03-22 21:38:45 +0300

    database

---
+++ order_service/app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+from app.core.config import get_db_url
+
+DATABASE_URL = get_db_url()
+engine = create_async_engine(DATABASE_URL)
+Session = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase, AsyncAttrs):
+    pass
+
+async def get_db():
+    session = Session()
+    try:
+        yield session
+    finally:
+        await session.close()
