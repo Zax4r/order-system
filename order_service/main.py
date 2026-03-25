efa9d9b047defbf6ca10ddd3953fda95e461568e
Zax4r <zaharbedarik@gmail.com>
2026-03-25 23:18:21 +0300

    exception handler added

--- order_service/main.py
+++ order_service/main.py
@@ -1,10 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from app.routers.user import router as user_router
+from app.routers.product import router as product_router
+from app.routers.order import router as order_router
+from app.core.exceptions import AppException
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(product_router)
+app.include_router(order_router)
+
+
+@app.exception_handler(AppException)
+async def app_exception_handler(request: Request, exc: AppException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+
 
 @app.get('/')
 async def root():
-    return {'msg':'Hello world'}
+    return {'msg': 'Hello world'}
