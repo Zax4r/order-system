041711061118fe2b6e1aeae96f5ac5ba63fa4c98
Zax4r <zaharbedarik@gmail.com>
2026-03-25 14:20:53 +0300

    user router

--- order_service/main.py
+++ order_service/main.py
@@ -1,9 +1,10 @@
-import app.database
 from fastapi import FastAPI
-
+from app.routers.user import router as user_router
 
 app = FastAPI()
 
+app.include_router(user_router)
+
 @app.get('/')
 async def root():
-    return {'message':'Hello world'}
+    return {'msg':'Hello world'}
